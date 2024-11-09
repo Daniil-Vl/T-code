88584d2e6d04090794ab2e05af810a57248294ef
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-11-09 13:43:03 +0300

    main: init submission-service module

---
+++ submission-service/src/main/java/ru/tbank/submissionservice/SubmissionServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.tbank.submissionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmissionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubmissionServiceApplication.class, args);
+    }
+
+}
---
+++ submission-service/src/test/java/ru/tbank/submissionservice/SubmissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tbank.submissionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmissionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
