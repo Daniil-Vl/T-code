7e38aae2906b0ae4f65beddb95daee72191e6256
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-11-09 13:43:17 +0300

    main: init contest-service module

---
+++ contest-service/src/main/java/ru/tbank/contestservice/ContestServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.tbank.contestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContestServiceApplication.class, args);
+    }
+
+}
---
+++ contest-service/src/test/java/ru/tbank/contestservice/ContestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tbank.contestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
