d7e696a1a00982ae2e9c3c59df84de1e18a285a0
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-11-15 21:06:21 +0300

    submission/object-storage: add exception handler for s3 service

---
+++ submission-service/src/main/java/ru/tbank/submissionservice/exception/handlers/S3ExceptionHandler.java
@@ -0,0 +1,32 @@
+package ru.tbank.submissionservice.exception.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.tbank.submissionservice.dto.ApiErrorResponse;
+import ru.tbank.submissionservice.exception.InvalidTestCases;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
+
+@RestControllerAdvice
+public class S3ExceptionHandler {
+
+    @ExceptionHandler(NoSuchKeyException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiErrorResponse handleNoSuchKeyException(NoSuchKeyException e) {
+        return new ApiErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                "Object with given id do not found"
+        );
+    }
+
+    @ExceptionHandler(InvalidTestCases.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrorResponse handleInvalidTestCases(InvalidTestCases e) {
+        return new ApiErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                e.getMessage()
+        );
+    }
+
+}
