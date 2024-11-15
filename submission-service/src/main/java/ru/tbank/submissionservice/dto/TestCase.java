1d2a2053214aeb1cc9a8e2941d3bf2393abb98c9
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-11-15 21:57:56 +0300

    submission/object-storage: add constructors and equals, hashcode methods to TestCase

--- submission-service/src/main/java/ru/tbank/submissionservice/dto/TestCase.java
+++ submission-service/src/main/java/ru/tbank/submissionservice/dto/TestCase.java
@@ -1,10 +1,16 @@
 package ru.tbank.submissionservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Objects;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TestCase {
     private String input;
     private String output;
@@ -12,5 +18,17 @@ public class TestCase {
     @Override
     public String toString() {
         return "TestCase [input=" + input + ", output=" + output + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TestCase testCase)) return false;
+        return Objects.equals(input, testCase.getInput()) && Objects.equals(output, testCase.getOutput());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getInput(), getOutput());
     }
 }
