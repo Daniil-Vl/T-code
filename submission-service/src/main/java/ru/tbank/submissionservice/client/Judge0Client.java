3226a78611a300e1f3665e8a713aea5c6ca5b755
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-12-11 14:29:45 +0300

    submission-connect: remove unused methods from Judge0Client and add documentation

--- submission-service/src/main/java/ru/tbank/submissionservice/client/Judge0Client.java
+++ submission-service/src/main/java/ru/tbank/submissionservice/client/Judge0Client.java
@@ -1,23 +1,33 @@
 package ru.tbank.submissionservice.client;
 
-import reactor.core.publisher.Mono;
-import ru.tbank.submissionservice.dto.SubmissionRequestDTO;
-import ru.tbank.submissionservice.dto.SubmissionResult;
-import ru.tbank.submissionservice.dto.SubmissionToken;
-import ru.tbank.submissionservice.enums.Language;
+import ru.tbank.submissionservice.dto.submission.SubmissionRequestDTO;
+import ru.tbank.submissionservice.dto.submission.SubmissionResults;
+import ru.tbank.submissionservice.dto.submission.SubmissionToken;
+import ru.tbank.submissionservice.dto.language.Language;
 
 import java.util.List;
 
 public interface Judge0Client {
-    SubmissionToken submit(String sourceCode, int languageId, String stdin);
-
-    Mono<SubmissionResult> submitWaiting(String sourceCode, int languageId, String stdin);
-
+    /**
+     * Create batch submission and send it to judge0 for execution
+     *
+     * @param submissionRequests
+     * @return
+     */
     List<SubmissionToken> submitBatch(List<SubmissionRequestDTO> submissionRequests);
 
-    SubmissionResult getSubmissionResult(String submissionToken);
+    /**
+     * Retrieves results for tokens batch
+     *
+     * @param tokens
+     * @return
+     */
+    SubmissionResults getBatchSubmissionResults(List<SubmissionToken> tokens);
 
-    List<SubmissionResult> getBatchSubmissionResults(List<SubmissionToken> tokens);
-
+    /**
+     * Return available languages
+     *
+     * @return
+     */
     List<Language> getLanguages();
 }
